4fcbee8082b97e82069e60ffc92b7a1720a65049
Rick Poleshuck <RickPoleshuck@gmail.com>
2021-11-22 13:49:35 -0500

    test

--- example/test/flutter_semver_test.dart
+++ example/test/flutter_semver_test.dart
@@ -4,12 +4,30 @@
 // writing unit tests, visit
 // https://flutter.dev/docs/cookbook/testing/unit/introduction
 
+import 'package:flutter_semver/flutter_semver.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:pub_semver/pub_semver.dart';
 
 void main() {
-  group('Plus Operator', () {
-    test('should add two numbers together', () {
-      expect(1 + 1, 2);
+  group('increment', () {
+    test('minor', () {
+      expect(increment(Version.parse('1.0.0'), SemanticPart.minor).toString(), '1.1.0');
+      expect(increment(Version.parse('1.0.0+3'), SemanticPart.minor).toString(), '1.1.0');
+      expect(increment(Version.parse('1.1.0'), SemanticPart.minor).toString(), '1.2.0');
+    });
+    test('major', () {
+      expect(increment(Version.parse('1.0.0'), SemanticPart.major).toString(), '2.0.0');
+      expect(increment(Version.parse('1.0.0+3'), SemanticPart.major).toString(), '2.0.0');
+      expect(increment(Version.parse('1.1.0'), SemanticPart.major).toString(), '2.0.0');
+    });
+    test('patch', () {
+      expect(increment(Version.parse('0.0.0'), SemanticPart.patch).toString(), '0.0.1');
+      expect(increment(Version.parse('1.0.0+3'), SemanticPart.patch).toString(), '1.0.1');
+      expect(increment(Version.parse('1.1.0'), SemanticPart.patch).toString(), '1.1.1');
+    });
+    test('build', () {
+      expect(increment(Version.parse('0.0.1'), SemanticPart.build).toString(), '0.0.1+1');
+      expect(increment(Version.parse('2.0.1+3'), SemanticPart.build).toString(), '2.0.1+4');
     });
   });
 }
